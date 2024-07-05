920a76d4fd0f85a9793398fae5385fb090e5714e
priyanshu lohar <priyanshulohar1@gmail.com>
2024-07-05 23:38:39 +0530

    init

---
+++ src/config/settings.js
@@ -0,0 +1,8 @@
+export default {
+	title: `Equinox AI`,
+	description: `Empowering startups and businesses with cutting-edge AI automation and chatbot solutions.`,
+	url: `https://pylhr.vercel.app`, // No trailing slash!
+	name: `Equinox AI`, // The short name of the business or brand name. Used for things like the copyright in the footer.
+  enableThemeSwitcher: true,
+  showLittleSticksPlug: true, // Disable this if you want to remove the Little Sticks plug from the footer. (╯°□°)╯︵ ┻━┻
+};
